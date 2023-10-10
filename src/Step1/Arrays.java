54d5f4439da5e74720498866b3427bb26dfbc403
Nikhil Tiwari <nikhiltiwari0024@gmail.com>
2023-10-10 07:40:22 +0530

    day 3 of solving strivers dsa sheet

---
+++ src/Step1/Arrays.java
@@ -0,0 +1,15 @@
+package Step1;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 5;
+        nums[1] = 4;
+        nums[2] = 2;
+        nums[3] = 7;
+        nums[4] = 8;
+        nums[3] *= 2;
+        System.out.println(nums[3]);
+
+    }
+}
---
+++ src/Step1/Strings.java
@@ -0,0 +1,8 @@
+package Step1;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "nikhil";
+        System.out.println(name.length());
+    }
+}
